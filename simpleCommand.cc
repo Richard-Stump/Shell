5069543185656816ca3e10ea122652542e8c6fa6
Richard Neil Stump <rnstump@data.cs.purdue.edu>
2021-03-05 21:48:10 -0500

    \Total:                   0.0 of 70.0\

--- simpleCommand.cc
+++ simpleCommand.cc
@@ -31,11 +31,11 @@ void SimpleCommand::print() {
   std::cout << std::endl;
 }
 
-std::unique_ptr<char**> getArgv() {
+std::unique_ptr<char*[]> getArgv() {
   size_t argvCount = _arguments.size() + 1;
-  const char** argv = new char*[argvCount];
+  const char*[] argv = new char*[argvCount];
 
-  return std::unique_ptr<const char**> argv;
+  return std::unique_ptr<const char*[]]> argv;
 }
 
 //execute the simple command
