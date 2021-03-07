4c8914c118f28bd4e6de840c369188c8aab86c44
Richard Neil Stump <rnstump@data.cs.purdue.edu>
2021-03-07 17:47:52 -0500

    \Total:                   2.0 of 70.0\

--- y.tab.cc
+++ y.tab.cc
@@ -1315,7 +1315,7 @@ yyreduce:
   case 13:
 #line 85 "shell.y" /* yacc.c:1646  */
     {
-    printf("   Yacc: insert argument \"%s\"\n", (yyvsp[0].cpp_string)->c_str());
+    //printf("   Yacc: insert argument \"%s\"\n", $1->c_str());
     Command::_currentSimpleCommand->insertArgument( (yyvsp[0].cpp_string) );\
   }
 #line 1322 "y.tab.cc" /* yacc.c:1646  */
@@ -1324,7 +1324,7 @@ yyreduce:
   case 14:
 #line 92 "shell.y" /* yacc.c:1646  */
     {
-    printf("   Yacc: insert command \"%s\"\n", (yyvsp[0].cpp_string)->c_str());
+    //printf("   Yacc: insert command \"%s\"\n", $1->c_str());
     Command::_currentSimpleCommand = new SimpleCommand();
     Command::_currentSimpleCommand->insertArgument( (yyvsp[0].cpp_string) );
   }
