7e5a1e7e0e4e866d04e521e4fda5bf0808de55d0
Richard Neil Stump <rnstump@data.cs.purdue.edu>
2021-03-26 16:11:15 -0400

    \Part 3 Total:            12.0 of 28.0\

--- shell.cc
+++ shell.cc
@@ -309,6 +309,43 @@ void Shell::recursivelyExpandWildcards(std::string prefix, std::string suffix)
   printIndent(indent); fprintf(stderr, "newSuffix: \"%s\"\n", suffix.c_str());
 
   std::string regexStr = wildcardToRegex(component);
+  regex_t regex;
+  int code = regcomp(&regex, regexStr.c_str(), REG_EXTENDED | REG_NOSUB);
+  
+  // If there is an error compiling the regex
+  if(code != 0) {
+    //get the compilation error string and put it into a buffer
+    char errbuff[128]; errbuff[127] = '\0';
+    regerror(code, &regex, errbuff, 127);
+    
+    fprintf(stderr, "Bad wildcard regex\n%d: %s\n", code, errbuff);
+    ::exit(-1);
+  }
+
+  struct dirent** nameList;
+  int nameCount = scandir(".", &nameList, NULL, alphasort);
+  if(nameCount == -1) {
+    perror("Scandir error");
+    ::exit(-1);
+  }
+
+  for(int i = 0; i < nameCount; i++) { 
+    if(regexec(&regex, nameList[i]->d_name, 0, nullptr, 0) == 0) {
+
+      if(nameList[i]->d_name[0] == '.' && path[0] == '.') {
+        std::string* arg = new std::string(nameList[i]->d_name);
+        Command::_currentSimpleCommand->insertArgument(arg);
+      }
+      else if(nameList[i]->d_name[0] != '.') {
+        std::string* arg = new std::string(nameList[i]->d_name);
+        Command::_currentSimpleCommand->insertArgument(arg);
+      }
+      //rintf(stderr, "%s\n", nameList[nameCount]->d_name);
+    }
+
+    free(nameList[i]);
+
+  }
 
   indent -= in_plus;
 }
