a9b1bb15dfa2b2af14740b56c63378fe7e9803b4
Richard Neil Stump <rnstump@data.cs.purdue.edu>
2021-03-26 20:58:14 -0400

    \Part 3 Total:            12.0 of 28.0\

--- shell.cc
+++ shell.cc
@@ -330,7 +330,7 @@ void Shell::recursivelyExpandWildcards(std::string prefix, std::string suffix)
     dir = prefix;
 
   struct dirent** nameList;
-  int nameCount = scandir(prefix.c_str(), &nameList, NULL, alphasort);
+  int nameCount = scandir(dir.c_str(), &nameList, NULL, alphasort);
   if(nameCount == -1) {
     perror("Scandir error");
     ::exit(-1);
@@ -338,7 +338,7 @@ void Shell::recursivelyExpandWildcards(std::string prefix, std::string suffix)
 
   for(int i = 0; i < nameCount; i++) { 
     if(regexec(&regex, nameList[i]->d_name, 0, nullptr, 0) == 0) {
-      recursivelyExpandWildcards(prefix + '/' nameList[i]->d_name, suffix);
+      recursivelyExpandWildcards(prefix + '/' + nameList[i]->d_name, suffix);
     }
 
     free(nameList[i]);
